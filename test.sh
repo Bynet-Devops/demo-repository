e2d0d8dc1a296828c7f33fd5965e693a4c77b2e3
lironva <121827062+lironva@users.noreply.github.com>
2023-02-17 18:35:34 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Download the SonarQube Scanner CLI tool
+curl -L https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip --output sonar-scanner-cli.zip
+unzip sonar-scanner-cli.zip
+
+# Analyze a sample project
+./sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner \
+  -Dsonar.projectKey=my-project \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=<your-sonarqube-token>
